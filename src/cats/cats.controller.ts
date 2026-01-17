ce5a5f284abc721e703401784e0ec99b0e953f7f
pedrolgr <pedrolgr47@gmail.com>
2026-01-16 22:47:38 -0300

    learned about decorators, and use Req and Res decoractor as injection to control the request and response

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Req, Res } from '@nestjs/common'
+import type { Request, Response } from 'express';
 
 @Controller('cats')
 export class CatController {
 
     @Get()
-    findAll(): string {
-        return 'Returnin all cats';
+    findAll(@Req() req: Request): string {
+        console.log(req)
+        return 'All cats here';
     }
 
     @Get('names')
-    getNames(): string {
-        return 'Returnin cats names';
+    getNames(@Res() res:Response) {
+        res.send('Here is cats names')
     }
+
+    
 }
